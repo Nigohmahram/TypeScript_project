e11fb90940d8eb183d888dedf9868e48f6e28b3a
Nigohmahram <usmonovmaxsudovich@gmail.com>
2023-08-13 19:22:33 +0900

    TypeScript Vita project

--- y/src/App.tsx
+++ y/src/App.tsx
@@ -1,10 +1,50 @@
+import{ChangeEvent, useState} from 'react'
 import  "./home-module.css";
+import { IData } from './interfaces';
+import { data } from './constants';
 const App = (): JSX.Element => {
+
+  const [title, setTitle] = useState<string>('');
+  const [arr, setArr] = useState<IData[]>(data);
+
+  const changeHandler  = (evt: ChangeEvent<HTMLInputElement >) => {
+    setTitle(evt.target.value)
+  }
+
+  const handleSubmit = (): void => {
+    if(!title?.length) return;
+    const newData ={
+      title: title,
+      id: new Date().getTime(),
+      description: 'Description',
+    }
+    setArr([...arr, newData])
+
+    setTitle ('');
+    console.log(title);
+
+  }
+
+  const deleteItem = (id: number): void => {
+    const newData = arr.filter(c => c.id !=id);
+    setArr(newData)
+  }
   return ( <div className='todo'>
-    <h1 className='title'>APP Todo</h1>
+    <h1 className='title'>특별 목록</h1>
   <div className='button'>
-    <button>Add</button>
+    <button onClick={handleSubmit} className='button'>추가</button>
   </div>
+    <input  placeholder="Enter Todo" value={title} onChange={changeHandler} className='input'/>
+    <div className='card'>
+    {arr.map(c => (
+      <div key={c.id} className='cardItem'>
+        <p>{c.title}</p>
+        <div className='delBtn'>
+          <button onClick={() => deleteItem(c.id)}>삭제</button>
+        </div>
+      </div>
+    ))}
+    </div>
   </div>
   );
 };
---
+++ y/src/constants/index.ts
@@ -0,0 +1,27 @@
+import { IData } from "../interfaces";
+
+export const data: IData[] = [
+
+      {title: "Omar",
+      id: 1,
+      description: "Description",
+    },
+
+      {title: "Usman",
+      id: 2,
+      description: "Description",
+    },
+
+      {title: "Abdulloh",
+      id: 3,
+      description: "Description",
+    },
+      {title: "Abdurahmon",
+      id: 4,
+      description: "Description",
+    },
+      {title: "Jahongir",
+      id: 5,
+      description: "Description",
+    },
+    ];
