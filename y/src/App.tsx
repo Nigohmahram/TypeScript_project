8c35f81deaea009e8e335674696ce9f149dfdbb0
Nigohmahram <usmonovmaxsudovich@gmail.com>
2023-08-13 15:49:22 +0900

    Add style project

--- y/src/App.tsx
+++ y/src/App.tsx
@@ -1,4 +1,11 @@
-const App = () => {
-  return <div>App</div>
-}
+import  "./home-module.css";
+const App = (): JSX.Element => {
+  return ( <div className='todo'>
+    <h1 className='title'>APP Todo</h1>
+  <div className='button'>
+    <button>Add</button>
+  </div>
+  </div>
+  );
+};
 export default App;
